34f80613a6f1f1b16ad7d36cde53f700ab89d397
Juan Jimenez <bozz212@outlook.com>
2022-08-17 14:46:43 -0700

    removed display file

--- W2/app.js
+++ W2/app.js
@@ -20,4 +20,35 @@ displayOnPage('Time to get up ---> 8am')
 displayOnPage('Time to eat ---> 12pm')
 displayOnPage('Time to work out ---> 6pm')
 
+// Displays a variable and a string.
+//  Displays a Scope 
+// Displays a Boolean
+// if else statement 
+//let timeToWorkOut1 = '6pm';
+// local 
+// GLobal
+let name = 'JuanJ'
+
+
+if (timeToSleep) {
+    console.log(`Since`, name, `goes to bed at 10pm he is a True Champion!`)
+}
+
+if (timeToGetUp = '8am') {
+    let isSmart = name === 'JuanJ';
+    console.log(`Since`, name, `Gets up at 8am he is a true Champion!`)
+} 
+
+if (timeToEat = '12pm') {
+    console.log(`Since`, name, `eats at 12pm he is a true Champion!`)
+} else {
+    console.log('misses out on knowledge.')
+}
+
+if (timeToWorkOut = '6pm') {
+    console.log(`Since`, name, `workouts at 6pm he is a Champion!`)
+} else {
+    console.log('Is Weak!')
+}
+
 console.log(`I prepare to eat at`, timeToEat + `.`)
