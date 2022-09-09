bf0d095bcff2cfcea242056c8b12f694ae1063ae
Juan Jimenez <bozz212@gmail.com>
2022-09-09 15:51:26 -0700

    new update with arrays and methods converted to functions

--- W5/app.js
+++ W5/app.js
@@ -4,59 +4,43 @@ const displayOnPage = function (text) {
   const outputDiv = document.getElementById("output")
   outputDiv.append(newParagraph)
 }
-let myGym = {
-  title: "Crunch",
+let myGym = [{
+  title: "Crunch Fitness",
   location: "Fresno",
-  members: 326,
-  roomAvailable: function (gymSize) {
-    let availableRoom = this.availableSpots - this.memberships;
-    return gymSize <= availableRoom;
-   },
-  gymRoom: function (gymSize) {
-    this.memberships = this.memberships + gymSize;
-  },
-  removeMembers: function (gymSize) {
-    this.memberships = this.memberships - gymSize;
-  },
-  
-}
-let otherGym = {
+  members: 326,}]
+ let otherGyms = [{
   title: "Gb3",
   location: "Fresno",
   members: 723,
-};
-if (otherGym.members >= 500 && otherGym.location === "Fresno") {
+}];
+displayOnPage(`Hello Customer! ${name}`);
+if (otherGyms.members >= 500 && otherGyms.location === "Fresno") {
   displayOnPage("I might go to the gym.");
 } else {
   displayOnPage("decide?");
-
 }
-let result;
-let circle = function (num) {
+
+let guess = function (num) {
   let result = num;
   return result;
 };
-let value = circle(7);
-let otherValue = circle(4);
+let value = guess(7);
+let otherValue = guess(4);
 displayOnPage(value + " " + `days a week of workout!`);
 
-displayOnPage(`Hello Customer! ${name}`);
-let getSummary = function (gym) {
-  return {
-    summary: `${otherGym.title} has ${otherGym.members} members.`,
-    membersSummary: `${myGym.title} has ${myGym.members} members.`,
-  };
-};
+displayOnPage(otherGyms.length + myGym.length + ' ' + `Gyms to Choose from.`);
 
-let gymSummary = getSummary(myGym, otherGym);
-let otherBookSummary = getSummary(otherGym);
-let summary = getSummary(myGym); 
+const index = otherGyms.findIndex(function (otherGym, index ) {
+  displayOnPage(`Gym I should Attend, ${otherGym.title}, ${otherGym.location}.`)
+  displayOnPage(`${otherGym.title} has ${otherGym.members} members?`)
+  return otherGym.title, myGym.title === 'Gym I should Attend'
+})
+const index2 = myGym.findIndex(function (myGym, index2 ) {
+  displayOnPage(`Other Gym I should Attend, ${myGym.title}, ${myGym.location}.`)
+  displayOnPage(`${myGym.title} has ${myGym.members} members?`)
+  return myGym.title === 'Other Gym I should Attend'
+})
 
-displayOnPage(gymSummary.membersSummary);
-displayOnPage(gymSummary.summary);
-myGym.gymRoom(52);
-displayOnPage(myGym.roomAvailable(4));
-myGym.removeMembers(5);
-displayOnPage(myGym.roomAvailable(5));
+
 
 
