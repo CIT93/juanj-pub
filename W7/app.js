b565d7fb0ef8c9c1d10c1776fc48748eb6e36b9f
Juan Jimenez <bozz212@gmail.com>
2022-09-22 23:43:05 -0700

    new updated W7

--- W7/app.js
+++ W7/app.js
@@ -1,53 +1,39 @@
-const displayOnPage = function (text) {
-  const newParagraph = document.createElement("P");
-  newParagraph.innerHTML = text;
-  const outputDiv = document.getElementById("output");
-  outputDiv.append(newParagraph);
-};
-let myGym = [
+const myGyms = [
   {
     title: "Crunch Fitness",
     location: "Fresno",
     members: 326,
+    verify: true
   },
-];
-let otherGyms = [
   {
     title: "Gb3",
     location: "Fresno",
     members: 723,
+    verify: true
   },
+  {
+    title: "Planet Fitness",
+    location: "Fresno",
+    memebers: 255,
+    verify: false
+  },
+  {
+    title: "Golds Gym",
+    location: "Los Angeles",
+    members: 1000,
+    verify: true
+  }
 ];
-displayOnPage(`Hello Customer! ${name}`);
-const gymTime = function () {
-if (otherGyms.members >= 500 && otherGyms.location === "Fresno") {
-  displayOnPage("I might go to the gym.");
-} else {
-  displayOnPage("decide?");
-}
-return gymTime
-} 
-let guess = function (num) {
-  let result = num;
-  return result;
-};
-let value = guess(7);
-let otherValue = guess(4);
-displayOnPage(value + " " + `days a week of workout!`);
 
-displayOnPage(otherGyms.length + myGym.length + " " + `Gyms to Choose from.`);
 
-const index = otherGyms.findIndex(function (otherGym, index) {
-  displayOnPage(
-    `Gym I should Attend, ${otherGym.title}, ${otherGym.location}.`
-  );
-  displayOnPage(`${otherGym.title} has ${otherGym.members} members?`);
-  return otherGym.title, myGym.title === "Gym I should Attend";
-});
-const index2 = myGym.findIndex(function (myGym, index2) {
-  displayOnPage(
-    `Other Gym I should Attend, ${myGym.title}, ${myGym.location}.`
-  );
-  displayOnPage(`${myGym.title} has ${myGym.members} members?`);
-  return myGym.title === "Other Gym I should Attend";
-});
+const falsemyGyms = myGyms.filter(function (myGym) {
+    return myGym.verify
+})
+const summary = document.createElement('h2')
+summary.textContent = `You have ${falsemyGyms.length} gyms to choose from.`
+document.querySelector('body').appendChild(summary)
+
+const newParagraph = document.createElement('p')
+newParagraph.textContent = `Choose a gym from ${myGyms[0].title}, ${myGyms[1].title}, or ${myGyms[3].title}.`
+document.querySelector('body').appendChild(newParagraph)
+
